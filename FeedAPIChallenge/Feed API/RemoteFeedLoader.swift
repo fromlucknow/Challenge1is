b8285917786ab4c56fb0ba0585b7636f8ca7dd4c
Saifur <saifurrahman3126@gmail.com>
2020-11-21 08:21:47 +0530

    delivering connectivity error on Client

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -20,7 +20,7 @@ public final class RemoteFeedLoader: FeedLoader {
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { (result) in
-            
+            completion(.failure(Error.connectivity))
         }
     }
 }
